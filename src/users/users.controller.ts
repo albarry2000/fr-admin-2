45cb71bba058f3dae55cddb05672f12aa6d45c20
Albarry <color.ui>
2022-10-25 20:58:47 +0200

    Create

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,24 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get, Body, Post } from '@nestjs/common';
+import { User } from './user.entity';
 
+const users : User[] = [
+    {
+        id: 0,
+        lastname: 'Doe',
+        firstname: 'John'
+    }
+]
 @Controller('users')
 export class UsersController {
 @Get('all')
 getAll(): string[] {
     return ['a', 'b', 'c'];
+}
+@Post()
+create(@Body() input: any): User {
+    const newUser= new User(users.length, input.firstname,input.lastname)
+    users.push(newUser)
+    return newUser
 }
 
 }
