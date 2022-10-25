81d6045f3e527a092813be2efbcfc6d3321a3dc2
Albarry <color.ui>
2022-10-25 20:55:43 +0200

    User Model

---
+++ src/users/user.entity.ts
@@ -0,0 +1,11 @@
+export class User{
+    id:number
+    firstname: String
+    lastname: String
+    
+    constructor(id:number, firstname: String, lastname: String){
+        this.id=id
+        this.firstname=firstname
+        this.lastname=lastname
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,8 +2,9 @@ import { Controller,Get } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-@Get()
-    getAll(): string[] {
-        return ['a', 'b', 'c'];
-    }
+@Get('all')
+getAll(): string[] {
+    return ['a', 'b', 'c'];
+}
+
 }
