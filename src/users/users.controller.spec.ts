3232b85c5cd5d52bca86982321781d80ecd0f4b5
Albarry <color.ui>
2022-11-08 08:31:35 +0100

    Test sur Users_ resolution probleme1

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,14 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService]
     }).compile();
 
+    service = module.get<UsersService>(UsersService);
     controller = module.get<UsersController>(UsersController);
   });
 
